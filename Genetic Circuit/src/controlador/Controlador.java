7df1c46bc2e7be7c774d2cd70c7c647b4f78331b
LordPotat <lordpotat@gmail.com>
2023-06-03 15:41:27 +0200

    Refactorizaciones en controlador

--- Genetic Circuit/src/controlador/Controlador.java
+++ Genetic Circuit/src/controlador/Controlador.java
@@ -84,6 +84,9 @@ public class Controlador {
 	 * @throws InterruptedException 
 	 */
 	public void iniciarControlador() throws InterruptedException  {
+		/* Como el programa se acaba de iniciar y todavía no empieza el proceso, actualiza el estado
+		 * a "en espera" para que directamente se muestre en la ventana cuando se cargue
+		 */
 		estado = Estado.EN_ESPERA;
 		//Genera las dos ventanas que formarán la interfaz gráfica del usuario
 		vista = new Vista(this);
@@ -98,22 +101,6 @@ public class Controlador {
 		ejecutarModoEditorCircuitos(ventana);
 		//Se carga el circuito por defecto al inicio del programa establecido en una constante
 		iniciarCircuito(CIRCUITO_INICIAL ,vista.getVentana());
-	}
-
-	/** 
-	 * Inicia los objetos del modelo de datos presentes en el circuito: la meta y obstaculos .
-	 * Obtiene los datos almacenados en el fichero correspondiente al circuito pasado como
-	 * argumento para poder asignar los parámetros necesarios para su inicialización
-	 * @param nombreCircuito: el circuito que debe cargarse 
-	 * @param ventana gráfica necesaria para poder dar contexto a los elementos
-	 */
-	public void iniciarCircuito(String nombreCircuito, Ventana ventana) {
-		//Obtiene el circuito desde su fichero de la carpeta del proyecto
-		modelo.setCircuito(Circuito.cargarCircuito(nombreCircuito));
-		//Inicia la meta y obstáculos a partir de los parámetros del circuito cargado
-		Circuito circuito = modelo.getCircuito();
-		modelo.setMeta(circuito.setupMeta(ventana));
-		modelo.setObstaculos(circuito.setupObstaculos(ventana));
 	}
 
 	/** 
@@ -135,6 +122,22 @@ public class Controlador {
 			System.exit(0);
 		}
 	}
+	
+	/** 
+	 * Inicia los objetos del modelo de datos presentes en el circuito: la meta y obstaculos .
+	 * Obtiene los datos almacenados en el fichero correspondiente al circuito pasado como
+	 * argumento para poder asignar los parámetros necesarios para su inicialización
+	 * @param nombreCircuito: el circuito que debe cargarse 
+	 * @param ventana gráfica necesaria para poder dar contexto a los elementos
+	 */
+	public void iniciarCircuito(String nombreCircuito, Ventana ventana) {
+		//Obtiene el circuito desde su fichero de la carpeta del proyecto
+		modelo.setCircuito(Circuito.cargarCircuito(nombreCircuito));
+		//Inicia la meta y obstáculos a partir de los parámetros del circuito cargado
+		Circuito circuito = modelo.getCircuito();
+		modelo.setMeta(circuito.setupMeta(ventana));
+		modelo.setObstaculos(circuito.setupObstaculos(ventana));
+	}
 
 	/**
 	 * Realiza todo el proceso evolutivo de la población. Comprueba si se ha cumplido
@@ -151,62 +154,103 @@ public class Controlador {
 		 * si es el caso después de mostrar la ruta óptima, para no continuar el proceso
 		 */
 		if(entidades.isObjetivoCumplido()) {
-			//Actualiza el estado a "finalizado" ya que el proceso evolutivo ha terminado
-			estado = Estado.FINALIZADO;
-			//Deshabilita el botón de pausa ya que no está en el proceso ya
-			panelControl.getBtnPausar().setEnabled(false);
-			panelControl.setValor("Generacion", entidades.getNumGeneraciones());
-			mostrarRutaOptima(entidades.getMejorEntidad());
+			finalizarProcesoEvolutivo(entidades, panelControl);
 			return;
 		}
 		//Número de frames que han pasado desde el inicio de su ciclo de vida
 		int numFramesGen = ventana.getNumFramesGen();
 		//Si todavía le queda tiempo de vida a la población, realiza un ciclo de ejecución
 		if(numFramesGen < tiempoVidaCache) {
-			entidades.realizarCiclo();
-			//En cada ciclo debe monitorizar los datos de la entidad monitorizada, si es que hay
-			if(entidades.getEntidadMonitorizada() != null) {
-				monitorizarEntidad(entidades.getEntidadMonitorizada());
-			}
-			ventana.setNumFramesGen(++numFramesGen);
+			continuarCicloVida(entidades, ventana, numFramesGen);
 		} 
 		//De lo contrario, la población debe evolucionar y reiniciar su ciclo de vida
 		else {
-			ventana.setNumFramesGen(0);
-			entidades.evolucionar();
-			/* Almacena temporalmente el valor del tiempo de vida modificado en medio del proceso,
-			 * para que no intenten aplicar genes que están fuera de su rango y espere a que se
-			 * reproduzcan para aplicarle el cambio
-			 */
-			tiempoVidaCache = entidades.getTiempoVida();
-			/* Si el modo automático no está activado y todavía no se ha cumplido el objetivo, 
-			 * para el proceso hasta que se pulse el botón de "siguiente". Si está activado,
-			 * deja que continúe por su cuenta.
-			 */
-			if(!modoAutomatico && !entidades.isObjetivoCumplido()) {
-				/* Actualiza el estado a "en espera" ya que se ha parado el proceso evolutivo 
-				 * hasta que pulsemos el botón de "siguiente" */
-				estado = Estado.EN_ESPERA;
-				parado = true; 
-				//Desabilita el botón de pausa para que no pueda iniciar otra generación por error
-				vista.getPanelControl().getBtnPausar().setEnabled(false);
-				/* Habilita el botón de "siguiente" para que pueda pasar a la siguiente generación
-				 * Lo hace con un delay para que se actualice correctamente el contador de generaciones */
-				new Timer().schedule(new TimerTask() {
-					@Override
-					public void run() {
-						vista.getPanelControl().getBtnProceder().setEnabled(true);
-					}
-				}, 5); 
-			} else {
-				//Si el modo automático está activado debe dejar de monitorizar sólo
-				limpiarEntidadMonitorizada();
-				limpiarUltimaGeneracion();
-			}
-			
+			terminarCicloVida(entidades, ventana);
 		}
+		//Muestra en el panel de control la generación en la que se encuentra al acabar el ciclo
 		panelControl.setValor("Generacion", entidades.getNumGeneraciones());
 		return;
+	}
+	
+	/**
+	 * Ordena a las entidades que prosigan su ejecución y muestra y actualiza la información
+	 * que considere necesaria 
+	 * @param entidades que deben proceder con su ciclo de vida
+	 * @param ventana cuyo contador de frames debe incrementarse
+	 * @param numFramesGen numero de frames que llevan vivos las entidades
+	 */
+	private void continuarCicloVida(Poblacion entidades, Ventana ventana, int numFramesGen) {
+		entidades.realizarCiclo();
+		//En cada ciclo debe monitorizar los datos de la entidad monitorizada, si es que hay
+		if(entidades.getEntidadMonitorizada() != null) {
+			monitorizarEntidad(entidades.getEntidadMonitorizada());
+		}
+		//Incrementa el contador de frames para que las entidades actuén según les toca
+		ventana.setNumFramesGen(++numFramesGen);
+	}
+	
+	/**
+	 * Realiza los últimos pasos en el ciclo de vida de la población para que el proceso
+	 * evolutivo continúe generando una nueva generación. Después dicta a los componentes
+	 * de la vista lo que deben hacer como preparación para la siguiente generación
+	 * @param entidades cuyo ciclo de vida termina y deben evolucionar en una generación nueva
+	 * @param ventana cuyo contador de frames debe reiniciarse para la siguiente generación
+	 */
+	private void terminarCicloVida(Poblacion entidades, Ventana ventana) {
+		ventana.setNumFramesGen(0);
+		entidades.evolucionar();
+		/* Almacena temporalmente el valor del tiempo de vida modificado en medio del proceso,
+		 * para que no intenten aplicar genes que están fuera de su rango y espere a que se
+		 * reproduzcan para aplicarle el cambio
+		 */
+		tiempoVidaCache = entidades.getTiempoVida();
+		/* Si el modo automático no está activado y todavía no se ha cumplido el objetivo, 
+		 * para el proceso hasta que se pulse el botón de "siguiente". Si está activado,
+		 * deja que continúe por su cuenta.
+		 */
+		if(!modoAutomatico && !entidades.isObjetivoCumplido()) {
+			detenerProceso(); 
+		} else {
+			//Si el modo automático está activado debe dejar de monitorizar sólo
+			limpiarEntidadMonitorizada();
+			limpiarUltimaGeneracion();
+		}
+	}
+	
+	/**
+	 * Para el proceso evolutivo hasta que el usuario de la orden de reanudarlo y
+	 * realiza los cambios en el panel de control para preparar la siguiente acción
+	 */
+	private void detenerProceso() {
+		/* Actualiza el estado a "en espera" ya que se ha parado el proceso evolutivo 
+		 * hasta que pulsemos el botón de "siguiente" */
+		estado = Estado.EN_ESPERA;
+		parado = true; 
+		//Desabilita el botón de pausa para que no pueda iniciar otra generación por error
+		vista.getPanelControl().getBtnPausar().setEnabled(false);
+		/* Habilita el botón de "siguiente" para que pueda pasar a la siguiente generación
+		 * Lo hace con un delay para que se actualice correctamente el contador de generaciones */
+		new Timer().schedule(new TimerTask() {
+			@Override
+			public void run() {
+				vista.getPanelControl().getBtnProceder().setEnabled(true);
+			}
+		}, 5);
+	}
+	
+	/**
+	 * Muestra la información que debe aparecer cuando el proceso evolutivo ha cumplido
+	 * su objetivo, como la ruta óptima alcanzada en la ventana
+	 * @param entidades: población de donde obtiene la entidad y datos a mostrar
+	 * @param panelControl 
+	 */
+	private void finalizarProcesoEvolutivo(Poblacion entidades, PanelControl panelControl) {
+		//Actualiza el estado a "finalizado" ya que el proceso evolutivo ha terminado
+		estado = Estado.FINALIZADO;
+		//Deshabilita el botón de pausa ya que no está en el proceso ya
+		panelControl.getBtnPausar().setEnabled(false);
+		panelControl.setValor("Generacion", entidades.getNumGeneraciones());
+		mostrarRutaOptima(entidades.getMejorEntidad());
 	}
 	
 	/**
@@ -239,7 +283,6 @@ public class Controlador {
 			}
 		}
 	}
-	
 	
 	/**
 	 * Actualiza un campo (label) del panel de control con un nuevo valor
@@ -289,7 +332,7 @@ public class Controlador {
 	}
 
 	/**
-	 * Actualizae en el panel de control todos los datos sobre una entidad
+	 * Actualiza en el panel de control todos los datos sobre una entidad
 	 * que está siendo monitorizada actualmente
 	 * @param entidad 
 	 */
